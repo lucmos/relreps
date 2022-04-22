1333b3bf222920ae9468baee607382b9510feaac
Luca Moschella <luca.moschella94@gmail.com>
2022-04-22 10:58:27 +0200

    Add script to test same class anchors

--- multirun_rae_commands.sh
+++ multirun_rae_commands.sh
@@ -7,148 +7,51 @@ python src/rae/run.py \
     nn.module.autoencoder._target_=rae.modules.rae.RAE \
     +nn.module.autoencoder.normalize_latents=True \
     nn.data.anchors_mode=fixed \
-    core.tags='[rae]' \
-    train.seed_index=0
-
-python src/rae/run.py \
-    nn.module._target_=rae.pl_modules.pl_variational.LightningVariational \
-    nn.module.autoencoder._target_=rae.modules.rae.RAE \
-    +nn.module.autoencoder.normalize_latents=True \
-    nn.data.anchors_mode=random_images \
-    core.tags='[rae]' \
-    train.seed_index=0
-
-python src/rae/run.py \
-    nn.module._target_=rae.pl_modules.pl_variational.LightningVariational \
-    nn.module.autoencoder._target_=rae.modules.rae.RAE \
-    +nn.module.autoencoder.normalize_latents=True \
-    nn.data.anchors_mode=random_latents \
-    core.tags='[rae]' \
-    train.seed_index=0
+    core.tags='[rae, same-class-anchors]' \
+    train.seed_index=0 \
+    nn.data.anchors_idx='[0, 17, 26, 34, 36, 41, 60, 64, 70, 75]'
 
 python src/rae/run.py \
     nn.module._target_=rae.pl_modules.pl_variational.LightningVariational \
     nn.module.autoencoder._target_=rae.modules.rae.RAE \
     +nn.module.autoencoder.normalize_latents=False \
     nn.data.anchors_mode=fixed \
-    core.tags='[rae]' \
-    train.seed_index=0
-
-python src/rae/run.py \
-    nn.module._target_=rae.pl_modules.pl_variational.LightningVariational \
-    nn.module.autoencoder._target_=rae.modules.rae.RAE \
-    +nn.module.autoencoder.normalize_latents=False \
-    nn.data.anchors_mode=random_images \
-    core.tags='[rae]' \
-    train.seed_index=0
-
-python src/rae/run.py \
-    nn.module._target_=rae.pl_modules.pl_variational.LightningVariational \
-    nn.module.autoencoder._target_=rae.modules.rae.RAE \
-    +nn.module.autoencoder.normalize_latents=False \
-    nn.data.anchors_mode=random_latents \
-    core.tags='[rae]' \
-    train.seed_index=0
-
-
-
+    core.tags='[rae, same-class-anchors]' \
+    train.seed_index=0 \
+    nn.data.anchors_idx='[0, 17, 26, 34, 36, 41, 60, 64, 70, 75]'
 
 python src/rae/run.py \
     nn.module._target_=rae.pl_modules.pl_variational.LightningVariational \
     nn.module.autoencoder._target_=rae.modules.rae.RAE \
     +nn.module.autoencoder.normalize_latents=True \
     nn.data.anchors_mode=fixed \
-    core.tags='[rae]' \
-    train.seed_index=1
-
-python src/rae/run.py \
-    nn.module._target_=rae.pl_modules.pl_variational.LightningVariational \
-    nn.module.autoencoder._target_=rae.modules.rae.RAE \
-    +nn.module.autoencoder.normalize_latents=True \
-    nn.data.anchors_mode=random_images \
-    core.tags='[rae]' \
-    train.seed_index=1
-
-python src/rae/run.py \
-    nn.module._target_=rae.pl_modules.pl_variational.LightningVariational \
-    nn.module.autoencoder._target_=rae.modules.rae.RAE \
-    +nn.module.autoencoder.normalize_latents=True \
-    nn.data.anchors_mode=random_latents \
-    core.tags='[rae]' \
-    train.seed_index=1
+    core.tags='[rae, same-class-anchors]' \
+    train.seed_index=1 \
+    nn.data.anchors_idx='[0, 17, 26, 34, 36, 41, 60, 64, 70, 75]'
 
 python src/rae/run.py \
     nn.module._target_=rae.pl_modules.pl_variational.LightningVariational \
     nn.module.autoencoder._target_=rae.modules.rae.RAE \
     +nn.module.autoencoder.normalize_latents=False \
     nn.data.anchors_mode=fixed \
-    core.tags='[rae]' \
-    train.seed_index=1
-
-python src/rae/run.py \
-    nn.module._target_=rae.pl_modules.pl_variational.LightningVariational \
-    nn.module.autoencoder._target_=rae.modules.rae.RAE \
-    +nn.module.autoencoder.normalize_latents=False \
-    nn.data.anchors_mode=random_images \
-    core.tags='[rae]' \
-    train.seed_index=1
-
-python src/rae/run.py \
-    nn.module._target_=rae.pl_modules.pl_variational.LightningVariational \
-    nn.module.autoencoder._target_=rae.modules.rae.RAE \
-    +nn.module.autoencoder.normalize_latents=False \
-    nn.data.anchors_mode=random_latents \
-    core.tags='[rae]' \
-    train.seed_index=1
-
-
-
-
+    core.tags='[rae, same-class-anchors]' \
+    train.seed_index=1 \
+    nn.data.anchors_idx='[0, 17, 26, 34, 36, 41, 60, 64, 70, 75]'
 
 python src/rae/run.py \
     nn.module._target_=rae.pl_modules.pl_variational.LightningVariational \
     nn.module.autoencoder._target_=rae.modules.rae.RAE \
     +nn.module.autoencoder.normalize_latents=True \
     nn.data.anchors_mode=fixed \
-    core.tags='[rae]' \
-    train.seed_index=2
-
-python src/rae/run.py \
-    nn.module._target_=rae.pl_modules.pl_variational.LightningVariational \
-    nn.module.autoencoder._target_=rae.modules.rae.RAE \
-    +nn.module.autoencoder.normalize_latents=True \
-    nn.data.anchors_mode=random_images \
-    core.tags='[rae]' \
-    train.seed_index=2
-
-python src/rae/run.py \
-    nn.module._target_=rae.pl_modules.pl_variational.LightningVariational \
-    nn.module.autoencoder._target_=rae.modules.rae.RAE \
-    +nn.module.autoencoder.normalize_latents=True \
-    nn.data.anchors_mode=random_latents \
-    core.tags='[rae]' \
-    train.seed_index=2
+    core.tags='[rae, same-class-anchors]' \
+    train.seed_index=2 \
+    nn.data.anchors_idx='[0, 17, 26, 34, 36, 41, 60, 64, 70, 75]'
 
 python src/rae/run.py \
     nn.module._target_=rae.pl_modules.pl_variational.LightningVariational \
     nn.module.autoencoder._target_=rae.modules.rae.RAE \
     +nn.module.autoencoder.normalize_latents=False \
     nn.data.anchors_mode=fixed \
-    core.tags='[rae]' \
-    train.seed_index=2
-
-python src/rae/run.py \
-    nn.module._target_=rae.pl_modules.pl_variational.LightningVariational \
-    nn.module.autoencoder._target_=rae.modules.rae.RAE \
-    +nn.module.autoencoder.normalize_latents=False \
-    nn.data.anchors_mode=random_images \
-    core.tags='[rae]' \
-    train.seed_index=2
-
-python src/rae/run.py \
-    nn.module._target_=rae.pl_modules.pl_variational.LightningVariational \
-    nn.module.autoencoder._target_=rae.modules.rae.RAE \
-    +nn.module.autoencoder.normalize_latents=False \
-    nn.data.anchors_mode=random_latents \
-    core.tags='[rae]' \
-    train.seed_index=2
+    core.tags='[rae, same-class-anchors]' \
+    train.seed_index=2 \
+    nn.data.anchors_idx='[0, 17, 26, 34, 36, 41, 60, 64, 70, 75]'
