3eb3dfdc6d82b3e6154a773b4ed11a723104c35b
Luca Moschella <luca.moschella94@gmail.com>
2022-04-22 11:12:06 +0200

    Change label

--- multirun_rae_commands.sh
+++ multirun_rae_commands.sh
@@ -7,7 +7,7 @@ python src/rae/run.py \
     nn.module.autoencoder._target_=rae.modules.rae.RAE \
     +nn.module.autoencoder.normalize_latents=True \
     nn.data.anchors_mode=fixed \
-    core.tags='[rae, same-class-anchors]' \
+    core.tags='[rae, anchors-class-seven]' \
     train.seed_index=0 \
     nn.data.anchors_idxs='[15, 29, 38, 42, 52, 71, 79, 84, 91, 96]'
 
@@ -16,7 +16,7 @@ python src/rae/run.py \
     nn.module.autoencoder._target_=rae.modules.rae.RAE \
     +nn.module.autoencoder.normalize_latents=False \
     nn.data.anchors_mode=fixed \
-    core.tags='[rae, same-class-anchors]' \
+    core.tags='[rae, anchors-class-seven]' \
     train.seed_index=0 \
     nn.data.anchors_idxs='[15, 29, 38, 42, 52, 71, 79, 84, 91, 96]'
 
@@ -25,7 +25,7 @@ python src/rae/run.py \
     nn.module.autoencoder._target_=rae.modules.rae.RAE \
     +nn.module.autoencoder.normalize_latents=True \
     nn.data.anchors_mode=fixed \
-    core.tags='[rae, same-class-anchors]' \
+    core.tags='[rae, anchors-class-seven]' \
     train.seed_index=1 \
     nn.data.anchors_idxs='[15, 29, 38, 42, 52, 71, 79, 84, 91, 96]'
 
@@ -34,7 +34,7 @@ python src/rae/run.py \
     nn.module.autoencoder._target_=rae.modules.rae.RAE \
     +nn.module.autoencoder.normalize_latents=False \
     nn.data.anchors_mode=fixed \
-    core.tags='[rae, same-class-anchors]' \
+    core.tags='[rae, anchors-class-seven]' \
     train.seed_index=1 \
     nn.data.anchors_idxs='[15, 29, 38, 42, 52, 71, 79, 84, 91, 96]'
 
@@ -43,7 +43,7 @@ python src/rae/run.py \
     nn.module.autoencoder._target_=rae.modules.rae.RAE \
     +nn.module.autoencoder.normalize_latents=True \
     nn.data.anchors_mode=fixed \
-    core.tags='[rae, same-class-anchors]' \
+    core.tags='[rae, anchors-class-seven]' \
     train.seed_index=2 \
     nn.data.anchors_idxs='[15, 29, 38, 42, 52, 71, 79, 84, 91, 96]'
 
@@ -52,6 +52,6 @@ python src/rae/run.py \
     nn.module.autoencoder._target_=rae.modules.rae.RAE \
     +nn.module.autoencoder.normalize_latents=False \
     nn.data.anchors_mode=fixed \
-    core.tags='[rae, same-class-anchors]' \
+    core.tags='[rae, anchors-class-seven]' \
     train.seed_index=2 \
     nn.data.anchors_idxs='[15, 29, 38, 42, 52, 71, 79, 84, 91, 96]'
