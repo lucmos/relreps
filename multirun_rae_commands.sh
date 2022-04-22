4f0e3c6573f9a9016b4a5c151b871419b5d44df5
Luca Moschella <luca.moschella94@gmail.com>
2022-04-22 11:13:21 +0200

    Test rae with same class zero

--- multirun_rae_commands.sh
+++ multirun_rae_commands.sh
@@ -7,51 +7,51 @@ python src/rae/run.py \
     nn.module.autoencoder._target_=rae.modules.rae.RAE \
     +nn.module.autoencoder.normalize_latents=True \
     nn.data.anchors_mode=fixed \
-    core.tags='[rae, anchors-class-seven]' \
+    core.tags='[rae, anchors-class-zero]' \
     train.seed_index=0 \
-    nn.data.anchors_idxs='[15, 29, 38, 42, 52, 71, 79, 84, 91, 96]'
+    nn.data.anchors_idxs='[1, 21, 34, 37, 51, 56, 63, 68, 69, 75]'
 
 python src/rae/run.py \
     nn.module._target_=rae.pl_modules.pl_variational.LightningVariational \
     nn.module.autoencoder._target_=rae.modules.rae.RAE \
     +nn.module.autoencoder.normalize_latents=False \
     nn.data.anchors_mode=fixed \
-    core.tags='[rae, anchors-class-seven]' \
+    core.tags='[rae, anchors-class-zero]' \
     train.seed_index=0 \
-    nn.data.anchors_idxs='[15, 29, 38, 42, 52, 71, 79, 84, 91, 96]'
+    nn.data.anchors_idxs='[1, 21, 34, 37, 51, 56, 63, 68, 69, 75]'
 
 python src/rae/run.py \
     nn.module._target_=rae.pl_modules.pl_variational.LightningVariational \
     nn.module.autoencoder._target_=rae.modules.rae.RAE \
     +nn.module.autoencoder.normalize_latents=True \
     nn.data.anchors_mode=fixed \
-    core.tags='[rae, anchors-class-seven]' \
+    core.tags='[rae, anchors-class-zero]' \
     train.seed_index=1 \
-    nn.data.anchors_idxs='[15, 29, 38, 42, 52, 71, 79, 84, 91, 96]'
+    nn.data.anchors_idxs='[1, 21, 34, 37, 51, 56, 63, 68, 69, 75]'
 
 python src/rae/run.py \
     nn.module._target_=rae.pl_modules.pl_variational.LightningVariational \
     nn.module.autoencoder._target_=rae.modules.rae.RAE \
     +nn.module.autoencoder.normalize_latents=False \
     nn.data.anchors_mode=fixed \
-    core.tags='[rae, anchors-class-seven]' \
+    core.tags='[rae, anchors-class-zero]' \
     train.seed_index=1 \
-    nn.data.anchors_idxs='[15, 29, 38, 42, 52, 71, 79, 84, 91, 96]'
+    nn.data.anchors_idxs='[1, 21, 34, 37, 51, 56, 63, 68, 69, 75]'
 
 python src/rae/run.py \
     nn.module._target_=rae.pl_modules.pl_variational.LightningVariational \
     nn.module.autoencoder._target_=rae.modules.rae.RAE \
     +nn.module.autoencoder.normalize_latents=True \
     nn.data.anchors_mode=fixed \
-    core.tags='[rae, anchors-class-seven]' \
+    core.tags='[rae, anchors-class-zero]' \
     train.seed_index=2 \
-    nn.data.anchors_idxs='[15, 29, 38, 42, 52, 71, 79, 84, 91, 96]'
+    nn.data.anchors_idxs='[1, 21, 34, 37, 51, 56, 63, 68, 69, 75]'
 
 python src/rae/run.py \
     nn.module._target_=rae.pl_modules.pl_variational.LightningVariational \
     nn.module.autoencoder._target_=rae.modules.rae.RAE \
     +nn.module.autoencoder.normalize_latents=False \
     nn.data.anchors_mode=fixed \
-    core.tags='[rae, anchors-class-seven]' \
+    core.tags='[rae, anchors-class-zero]' \
     train.seed_index=2 \
-    nn.data.anchors_idxs='[15, 29, 38, 42, 52, 71, 79, 84, 91, 96]'
+    nn.data.anchors_idxs='[1, 21, 34, 37, 51, 56, 63, 68, 69, 75]'
