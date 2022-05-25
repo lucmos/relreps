146eb01353b95f1b2dc40153655e3fe245c5df75
Luca Moschella <luca.moschella94@gmail.com>
2022-05-25 18:28:32 +0200

    -

--- src/rae/pl_modules/pl_variational.py
+++ src/rae/pl_modules/pl_variational.py
@@ -20,7 +20,7 @@ class LightningVariational(LightningGAE):
             out[Output.LATENT_LOGVAR],
             variational_beta=self.hparams.loss.variational_beta,
         )
-        loss = loss - out[Output.INV_LATENTS].clamp(max=0).sum() * 1000
+        loss = loss - out[Output.INV_LATENTS].clamp(max=0).sum() * 100
 
         return {
             Output.LOSS: loss,
