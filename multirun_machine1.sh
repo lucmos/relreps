2f5f67257016487172ebe86b82effccda1148c60
Luca Moschella <luca.moschella94@gmail.com>
2022-05-25 17:52:07 +0200

    Setup machine1

--- multirun_machine1.sh
+++ multirun_machine1.sh
@@ -11,8 +11,9 @@ python src/rae/run.py \
     nn.module.autoencoder.normalize_only_anchors_means=False \
     nn.module.autoencoder.normalize_relative_embedding='off' \
     nn.module.autoencoder.relative_embedding_method=basis_change \
+    nn.module.autoencoder.force_positive_embeddings=True \
     core.tags='[rae]' \
-    train.seed_index=2 \
+    train.seed_index=0 \
     nn.data.anchors_mode=stratified_subset \
     nn.data.anchors_num=50 \
     nn.data.anchors_idxs=null \
@@ -28,8 +29,9 @@ python src/rae/run.py \
     nn.module.autoencoder.normalize_only_anchors_means=False \
     nn.module.autoencoder.normalize_relative_embedding='off' \
     nn.module.autoencoder.relative_embedding_method=basis_change \
+    nn.module.autoencoder.force_positive_embeddings=True \
     core.tags='[rae]' \
-    train.seed_index=3 \
+    train.seed_index=1 \
     nn.data.anchors_mode=stratified_subset \
     nn.data.anchors_num=50 \
     nn.data.anchors_idxs=null \
