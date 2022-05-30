b6b1845720030880a38e06653821ce0c92f17119
Luca Moschella <luca.moschella94@gmail.com>
2022-05-30 09:01:16 +0200

    Add naive CNN classifier

--- multirun_machine1.sh
+++ multirun_machine1.sh
@@ -2,35 +2,7 @@
 
 # Variational RAE
 python src/rae/run.py \
-    nn.module._target_=rae.pl_modules.pl_variational.LightningVariational \
-    nn.module.autoencoder._target_=rae.modules.rae.RAE \
-    nn.module.autoencoder.reparametrize_anchors=False \
-    nn.module.autoencoder.normalize_latents=off \
-    nn.module.autoencoder.normalize_only_anchors_latents=False \
-    nn.module.autoencoder.normalize_means=l2 \
-    nn.module.autoencoder.normalize_only_anchors_means=False \
-    nn.module.autoencoder.normalize_relative_embedding='off' \
-    nn.module.autoencoder.relative_embedding_method=basis_change \
-    core.tags='[rae]' \
-    train.seed_index=2 \
-    nn.data.anchors_mode=stratified_subset \
-    nn.data.anchors_num=50 \
-    nn.data.anchors_idxs=null \
-    nn.module.autoencoder.latent_dim=20
-
-python src/rae/run.py \
-    nn.module._target_=rae.pl_modules.pl_variational.LightningVariational \
-    nn.module.autoencoder._target_=rae.modules.rae.RAE \
-    nn.module.autoencoder.reparametrize_anchors=False \
-    nn.module.autoencoder.normalize_latents=off \
-    nn.module.autoencoder.normalize_only_anchors_latents=False \
-    nn.module.autoencoder.normalize_means=l2 \
-    nn.module.autoencoder.normalize_only_anchors_means=False \
-    nn.module.autoencoder.normalize_relative_embedding='off' \
-    nn.module.autoencoder.relative_embedding_method=basis_change \
-    core.tags='[rae]' \
-    train.seed_index=3 \
-    nn.data.anchors_mode=stratified_subset \
-    nn.data.anchors_num=50 \
-    nn.data.anchors_idxs=null \
-    nn.module.autoencoder.latent_dim=20
+    nn.module._target_=rae.pl_modules.pl_gclassifier.LightningClassifier \
+    nn.module.model._target_=rae.modules.classifier.CNN \
+    nn.module.model.hidden_channels=64 \
+    core.tags='[classifier, cnn]' \
