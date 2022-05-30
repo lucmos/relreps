e884988b0ca06e15448a40eb15110f8c9cd5264b
Luca Moschella <luca.moschella94@gmail.com>
2022-05-30 16:09:43 +0200

    Refactor classifiers to be independent to image sizes

--- src/rae/utils/tensor_ops.py
+++ src/rae/utils/tensor_ops.py
@@ -1,6 +1,7 @@
 from typing import Any
 
 import torch
+from torch import nn
 
 
 def detach_tensors(x: Any) -> Any:
@@ -8,3 +9,23 @@ def detach_tensors(x: Any) -> Any:
         return x.detach().cpu()
     else:
         return x
+
+
+def infer_dimension(width: int, height: int, n_channels: int, model: nn.Module, batch_size: int = 8) -> torch.Tensor:
+    """Compute the output of a model given a fake batch
+
+    Args:
+        width: the width of the image to generate the fake batch
+        height:  the height of the image to generate the fake batch
+        n_channels:  the n_channels of the image to generate the fake batch
+        model: the model to use to compute the output
+        batch_size: batch size to use for the fake output
+
+    Returns:
+        the fake output
+    """
+
+    with torch.no_grad():
+        fake_batch = torch.zeros([batch_size, n_channels, width, height])
+        fake_out = model(fake_batch)
+        return fake_out
