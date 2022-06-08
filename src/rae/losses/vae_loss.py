bb214daac63af069be884b0daf4326de5a6d1644
Luca Moschella <luca.moschella94@gmail.com>
2022-06-08 16:50:41 +0200

    Change reconstruction vae loss to mse by default

--- src/rae/losses/vae_loss.py
+++ src/rae/losses/vae_loss.py
@@ -7,12 +7,12 @@ class VaeLoss:
         self.variational_beta = variational_beta
 
     def __call__(self, image_pred: torch.Tensor, image_batch: torch.Tensor, mu: torch.Tensor, logvar: torch.Tensor):
+        # recon_loss = F.binary_cross_entropy(image_pred.view(-1, 784), image_batch.view(-1, 784), reduction="sum")
+        #
         recon_loss = F.mse_loss(
             image_pred,
             image_batch,
-            # image_pred.view(-1, image_pred.shape[2] * image_pred.shape[3]),
-            # image_batch.view(-1, image_batch.shape[2] * image_batch.shape[3]),
-            reduction="mean",
+            reduction="sum",
         )
 
         kldivergence = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
