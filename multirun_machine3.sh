d050f4f66d6652f3440ab21f029fdf1b1962a91c
Luca Moschella <luca.moschella94@gmail.com>
2022-07-01 10:13:18 +0200

    Setup machine3

--- multirun_machine3.sh
+++ multirun_machine3.sh
@@ -7,5 +7,48 @@ python src/rae/run.py \
   nn.module.model.finetune=True \
   nn.module.model.similarity_mode=inner \
   nn.module.model.similarities_quantization_mode=differentiable_round \
-  nn.module.model.similarities_bin_size=0.1
+  nn.module.model.similarities_bin_size=0.1 \
+  nn.module.model.transform_resnet_features=True
+
+# ResNet
+python src/rae/run.py \
+  core.tags='[resnet, pretrained, absolute]' \
+  nn/module/model=resnet \
+  nn.module.model.finetune=True \
+  nn.module.model.transform_resnet_features=True
+
+python src/rae/run.py \
+  core.tags='[resnet, pretrained, finetuning, quantization, relative]' \
+  nn/module/model=relresnet \
+  nn.module.model.finetune=True \
+  nn.module.model.similarity_mode=inner \
+  nn.module.model.similarities_quantization_mode=differentiable_round \
+  nn.module.model.similarities_bin_size=0.5 \
+  nn.module.model.transform_resnet_features=True
+
+python src/rae/run.py \
+  core.tags='[resnet, pretrained, finetuning, quantization, relative]' \
+  nn/module/model=relresnet \
+  nn.module.model.finetune=True \
+  nn.module.model.similarity_mode=inner \
+  nn.module.model.similarities_quantization_mode=differentiable_round \
+  nn.module.model.similarities_bin_size=0.25 \
+  nn.module.model.transform_resnet_features=True
+
+python src/rae/run.py \
+  core.tags='[resnet, pretrained, finetuning, quantization, relative]' \
+  nn/module/model=relresnet \
+  nn.module.model.finetune=True \
+  nn.module.model.similarity_mode=inner \
+  nn.module.model.similarities_quantization_mode=differentiable_round \
+  nn.module.model.similarities_bin_size=0.05 \
+  nn.module.model.transform_resnet_features=True
+
+python src/rae/run.py \
+  core.tags='[resnet, pretrained, finetuning, quantization, relative]' \
+  nn/module/model=relresnet \
+  nn.module.model.finetune=True \
+  nn.module.model.similarity_mode=inner \
+  nn.module.model.similarities_quantization_mode=differentiable_round \
+  nn.module.model.similarities_bin_size=0.005 \
   nn.module.model.transform_resnet_features=True
