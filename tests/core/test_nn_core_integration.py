0921677cbb6ab2daabf4238b26be3d9aeb070171
Luca Moschella <luca.moschella94@gmail.com>
2022-07-20 10:03:11 +0200

    Remove core tests

--- tests/core/test_nn_core_integration.py
+++
@@ -1,6 +0,0 @@
-from nn_core.common import PROJECT_ROOT
-
-
-def test_project_root() -> None:
-    assert PROJECT_ROOT
-    assert (PROJECT_ROOT / "conf").exists()
--- tests/core/test_training.py
+++
@@ -1,2 +0,0 @@
-def test_train_loop(run_trainings: str) -> None:
-    assert run_trainings
