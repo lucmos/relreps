af0c79e9a8e59c8568cbfd8a3805fe0517756f07
Luca Moschella <luca.moschella94@gmail.com>
2022-07-26 12:11:37 +0200

    Change tasks epochs

--- multirun_machine2.sh
+++ multirun_machine2.sh
@@ -2,18 +2,18 @@
 
 # Absolute
 
-#python src/rae/run.py \
-#  core.tags='[continual, absolute]' \
-#  nn.data.anchors_num=500 \
-#  nn/data/datasets=continual/cifar10 \
-#  nn/module=continual_classifier \
-#  nn/module/model=cnn \
-#  nn.module.model.hidden_features=512 \
-#  nn.module.model.dropout_p=0.5 \
-#  nn.module.replay.max_size=0
+python src/rae/run.py \
+  core.tags='[complete-bootstrap, continual, absolute]' \
+  nn.data.anchors_num=500 \
+  nn/data/datasets=continual/cifar10 \
+  nn/module=continual_classifier \
+  nn/module/model=cnn \
+  nn.module.model.hidden_features=512 \
+  nn.module.model.dropout_p=0.5 \
+  nn.module.replay.max_size=0
 
 python src/rae/run.py \
-  core.tags='[continual, absolute]' \
+  core.tags='[complete-bootstrap, continual, absolute]' \
   nn.data.anchors_num=500 \
   nn/data/datasets=continual/cifar10 \
   nn/module=continual_classifier \
@@ -24,18 +24,18 @@ python src/rae/run.py \
 
 # Relative
 
-#python src/rae/run.py \
-#  core.tags='[continual, relative]' \
-#  nn.data.anchors_num=500 \
-#  nn/data/datasets=continual/cifar10 \
-#  nn/module=continual_classifier \
-#  nn/module/model=rcnn \
-#  nn.module.model.hidden_features=512 \
-#  nn.module.model.dropout_p=0.5 \
-#  nn.module.replay.max_size=0
+python src/rae/run.py \
+  core.tags='[complete-bootstrap, continual, relative]' \
+  nn.data.anchors_num=500 \
+  nn/data/datasets=continual/cifar10 \
+  nn/module=continual_classifier \
+  nn/module/model=rcnn \
+  nn.module.model.hidden_features=512 \
+  nn.module.model.dropout_p=0.5 \
+  nn.module.replay.max_size=0
 
 python src/rae/run.py \
-  core.tags='[continual, relative]' \
+  core.tags='[complete-bootstrap, continual, relative]' \
   nn.data.anchors_num=500 \
   nn/data/datasets=continual/cifar10 \
   nn/module=continual_classifier \
@@ -44,27 +44,27 @@ python src/rae/run.py \
   nn.module.model.dropout_p=0.5 \
   nn.module.replay.max_size=100
 
-#python src/rae/run.py \
-#  core.tags='[continual, relative]' \
-#  nn.data.anchors_num=500 \
-#  nn/data/datasets=continual/cifar10 \
-#  nn/module=continual_classifier \
-#  nn/module/model=rcnn \
-#  nn.module.model.similarities_quantization_mode=differentiable_round \
-#  nn.module.model.similarities_bin_size=0.5 \
-#  nn.module.model.hidden_features=512 \
-#  nn.module.model.dropout_p=0.5 \
-#  nn.module.replay.max_size=0
-#
-#python src/rae/run.py \
-#  core.tags='[continual, relative]' \
-#  nn.data.anchors_num=500 \
-#  nn/data/datasets=continual/cifar10 \
-#  nn/module=continual_classifier \
-#  nn/module/model=rcnn \
-#  nn.module.model.similarities_quantization_mode=differentiable_round \
-#  nn.module.model.similarities_bin_size=0.5 \
-#  nn.module.model.hidden_features=512 \
-#  nn.module.model.dropout_p=0.5 \
-#  nn.module.replay.max_size=100
-#
+python src/rae/run.py \
+  core.tags='[complete-bootstrap, continual, relative]' \
+  nn.data.anchors_num=500 \
+  nn/data/datasets=continual/cifar10 \
+  nn/module=continual_classifier \
+  nn/module/model=rcnn \
+  nn.module.model.similarities_quantization_mode=differentiable_round \
+  nn.module.model.similarities_bin_size=0.5 \
+  nn.module.model.hidden_features=512 \
+  nn.module.model.dropout_p=0.5 \
+  nn.module.replay.max_size=0
+
+python src/rae/run.py \
+  core.tags='[complete-bootstrap, continual, relative]' \
+  nn.data.anchors_num=500 \
+  nn/data/datasets=continual/cifar10 \
+  nn/module=continual_classifier \
+  nn/module/model=rcnn \
+  nn.module.model.similarities_quantization_mode=differentiable_round \
+  nn.module.model.similarities_bin_size=0.5 \
+  nn.module.model.hidden_features=512 \
+  nn.module.model.dropout_p=0.5 \
+  nn.module.replay.max_size=100
+
