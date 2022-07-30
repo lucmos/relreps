9a633b09734c0b80c2276408130f87dc67174a39
Luca Moschella <luca.moschella94@gmail.com>
2022-07-30 17:59:37 +0200

    Add dropout layers

--- multirun_machine3.sh
+++ multirun_machine3.sh
@@ -8,7 +8,7 @@ python src/rae/run.py -m \
   nn/module/model=rcnn \
   nn.module.model.hidden_features=512 \
   nn.module.model.values_mode=similarities \
-  nn.module.model.num_subspaces='1,2,4,8,16' \
+  nn.module.model.num_subspaces='1,16' \
   nn.module.model.values_self_attention_nhead=null \
   nn.module.model.transform_elements=null \
   nn.module.model.normalization_mode=l2 \
@@ -18,4 +18,4 @@ python src/rae/run.py -m \
   nn.module.model.similarities_aggregation_mode=null \
   nn.module.model.similarities_aggregation_n_groups=null \
   nn.module.model.anchors_sampling_mode=null \
-  nn.module.model.n_anchors_sampling_per_class=null 
+  nn.module.model.n_anchors_sampling_per_class=null
