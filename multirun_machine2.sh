851abb163257cdeb4526755df8abb2d4109d1c82
Luca Moschella <luca.moschella94@gmail.com>
2022-08-14 12:08:33 +0200

    Add cifa100 no norm

--- multirun_machine2.sh
+++ multirun_machine2.sh
@@ -3,15 +3,14 @@
 
 # Absolute
 python src/rae/run.py -m \
-  core.tags='[reconstruction, absolute, tab1-reconstruction]' \
+  core.tags='[reconstruction, absolute, tab1-reconstruction, dev]' \
   'train.seed_index=0' \
   'nn/data/datasets=vision/cifar10_nonorm' \
   nn/module=autoencoder \
-  nn/module/model=vae2 \
+  nn/module/model=ae \
   train=reconstruction \
   nn.module.model.latent_dim=256 \
   "nn.module.model.hidden_dims=null" \
-  "nn.module.model.last_activation=sigmoid" \
   "nn.module.optimizer.lr=5e-4" \
   nn.data.anchors_num=500 \
   train.trainer.max_epochs=60
