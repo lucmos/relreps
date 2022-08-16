b290eb39f4ba01520404241cfbba4e25ac4d760c
Luca Moschella <luca.moschella94@gmail.com>
2022-08-16 15:09:39 +0200

    Setup machine 1

--- multirun_machine1.sh
+++ multirun_machine1.sh
@@ -1,121 +1,15 @@
 #!/bin/bash
 
-python src/rae/run.py \
-  core.tags='[resnet, pretrained, finetuning, quantization, relative, keys-queries-transform]' \
+# Absolute
+python src/rae/run.py -m \
+  core.tags='[reconstruction, absolute, fig:ae-rotations]' \
+  'nn/data/datasets=vision/mnist' \
+  'train.seed_index=0,1,2,3,4,5' \
+  nn/module=autoencoder \
+  nn/module/model=ae \
+  train=reconstruction \
+  nn.module.model.latent_dim=2 \
   nn.data.anchors_num=500 \
-  nn/module/model=relresnet \
-  nn.module.model.use_pretrained=True \
-  nn.module.model.finetune=True \
-  nn.module.model.transform_resnet_features=True \
-  nn.module.model.hidden_features=512 \
-  nn.module.model.dropout_p=0.5 \
-  nn.module.model.transform_elements='[attention_keys, attention_queries]' \
-  nn.module.model.normalization_mode=l2 \
-  nn.module.model.similarity_mode=inner \
-  nn.module.model.values_mode=similarities \
-  nn.module.model.similarities_quantization_mode=differentiable_round \
-  nn.module.model.similarities_bin_size=0.5 \
-  nn.module.model.similarities_aggregation_mode=null \
-  nn.module.model.similarities_aggregation_n_groups=null \
-  nn.module.model.anchors_sampling_mode=null \
-  nn.module.model.n_anchors_sampling_per_class=null
-
-python src/rae/run.py \
-  core.tags='[resnet, pretrained, finetuning, quantization, relative, keys-queries-transform]' \
-  nn.data.anchors_num=500 \
-  nn/module/model=relresnet \
-  nn.module.model.use_pretrained=True \
-  nn.module.model.finetune=True \
-  nn.module.model.transform_resnet_features=True \
-  nn.module.model.hidden_features=512 \
-  nn.module.model.dropout_p=0.5 \
-  nn.module.model.transform_elements='[attention_keys, attention_queries]' \
-  nn.module.model.normalization_mode=l2 \
-  nn.module.model.similarity_mode=inner \
-  nn.module.model.values_mode=similarities \
-  nn.module.model.similarities_quantization_mode=differentiable_round \
-  nn.module.model.similarities_bin_size=0.1 \
-  nn.module.model.similarities_aggregation_mode=null \
-  nn.module.model.similarities_aggregation_n_groups=null \
-  nn.module.model.anchors_sampling_mode=null \
-  nn.module.model.n_anchors_sampling_per_class=null
-
-python src/rae/run.py \
-  core.tags='[resnet, pretrained, finetuning, relative, keys-queries-transform]' \
-  nn.data.anchors_num=500 \
-  nn/module/model=relresnet \
-  nn.module.model.use_pretrained=True \
-  nn.module.model.finetune=True \
-  nn.module.model.transform_resnet_features=True \
-  nn.module.model.hidden_features=512 \
-  nn.module.model.dropout_p=0.5 \
-  nn.module.model.transform_elements='[attention_keys, attention_queries]' \
-  nn.module.model.normalization_mode=l2 \
-  nn.module.model.similarity_mode=inner \
-  nn.module.model.values_mode=similarities \
-  nn.module.model.similarities_quantization_mode=null \
-  nn.module.model.similarities_bin_size=null \
-  nn.module.model.similarities_aggregation_mode=null \
-  nn.module.model.similarities_aggregation_n_groups=null \
-  nn.module.model.anchors_sampling_mode=null \
-  nn.module.model.n_anchors_sampling_per_class=null
-
-python src/rae/run.py \
-  core.tags='[resnet, pretrained, finetuning, quantization, relative, anchors-sampling, keys-queries-transform]' \
-  nn.data.anchors_num=1500 \
-  nn/module/model=relresnet \
-  nn.module.model.use_pretrained=True \
-  nn.module.model.finetune=True \
-  nn.module.model.transform_resnet_features=True \
-  nn.module.model.hidden_features=512 \
-  nn.module.model.dropout_p=0.5 \
-  nn.module.model.transform_elements='[attention_keys, attention_queries]' \
-  nn.module.model.normalization_mode=l2 \
-  nn.module.model.similarity_mode=inner \
-  nn.module.model.values_mode=similarities \
-  nn.module.model.similarities_quantization_mode=differentiable_round \
-  nn.module.model.similarities_bin_size=0.5 \
-  nn.module.model.similarities_aggregation_mode=null \
-  nn.module.model.similarities_aggregation_n_groups=null \
-  nn.module.model.anchors_sampling_mode=stratified \
-  nn.module.model.n_anchors_sampling_per_class=3
-
-python src/rae/run.py \
-  core.tags='[resnet, pretrained, finetuning, quantization, relative, anchors-sampling, keys-queries-transform]' \
-  nn.data.anchors_num=1500 \
-  nn/module/model=relresnet \
-  nn.module.model.use_pretrained=True \
-  nn.module.model.finetune=True \
-  nn.module.model.transform_resnet_features=True \
-  nn.module.model.hidden_features=512 \
-  nn.module.model.dropout_p=0.5 \
-  nn.module.model.transform_elements='[attention_keys, attention_queries]' \
-  nn.module.model.normalization_mode=l2 \
-  nn.module.model.similarity_mode=inner \
-  nn.module.model.values_mode=similarities \
-  nn.module.model.similarities_quantization_mode=differentiable_round \
-  nn.module.model.similarities_bin_size=0.1 \
-  nn.module.model.similarities_aggregation_mode=null \
-  nn.module.model.similarities_aggregation_n_groups=null \
-  nn.module.model.anchors_sampling_mode=stratified \
-  nn.module.model.n_anchors_sampling_per_class=3
-
-python src/rae/run.py \
-  core.tags='[resnet, pretrained, finetuning, relative, anchors-sampling, keys-queries-transform]' \
-  nn.data.anchors_num=1500 \
-  nn/module/model=relresnet \
-  nn.module.model.use_pretrained=True \
-  nn.module.model.finetune=True \
-  nn.module.model.transform_resnet_features=True \
-  nn.module.model.hidden_features=512 \
-  nn.module.model.dropout_p=0.5 \
-  nn.module.model.transform_elements='[attention_keys, attention_queries]' \
-  nn.module.model.normalization_mode=l2 \
-  nn.module.model.similarity_mode=inner \
-  nn.module.model.values_mode=similarities \
-  nn.module.model.similarities_quantization_mode=null \
-  nn.module.model.similarities_bin_size=null \
-  nn.module.model.similarities_aggregation_mode=null \
-  nn.module.model.similarities_aggregation_n_groups=null \
-  nn.module.model.anchors_sampling_mode=stratified \
-  nn.module.model.n_anchors_sampling_per_class=3
+  "nn.module.model.hidden_dims=null" \
+  "nn.module.optimizer.lr=5e-4" \
+  train.trainer.max_epochs=40
