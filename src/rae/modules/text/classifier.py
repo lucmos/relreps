73be2c62d3dc160ead3a01b7d716e6b112945537
Valentino Maiorca <valentino@maiorca.xyz>
2022-09-08 23:44:03 +0200

    Move absolute classifiers in their own package

--- src/rae/modules/text/classifier.py
+++ src/rae/modules/text/classifier.py
@@ -1,4 +1,3 @@
-import functools
 import itertools
 import logging
 from typing import Any, Collection, Sequence, Set
@@ -181,6 +180,7 @@ class HFTextClassifier(nn.Module):
         batch_pre_reduce: Collection[EncodingLevel] = None,
         anchors_reduce: Collection[EncodingLevel] = None,
         batch_post_reduce: Collection[EncodingLevel] = None,
+        finetuning: bool = False,
         **kwargs,
     ) -> None:
         """Simple model that uses convolutions.
@@ -265,8 +265,8 @@ class HFTextClassifier(nn.Module):
                     anchor_encodings = self.call_transformer(
                         encodings=anchors_batch["encodings"], sample_ids=anchors_batch["index"]
                     )
-                    # (batch, hidden), (64, hidden),
                     self.anchor_encodings.append(anchor_encodings)
+
                 self.anchor_encodings = torch.cat(self.anchor_encodings, dim=0)
             anchors = self.anchor_encodings
             # anchors, _ = EncodingLevel.reduce(
@@ -304,104 +304,6 @@ class HFTextClassifier(nn.Module):
             Output.BATCH_LATENT: encoding[Output.BATCH_LATENT],
             Output.ANCHORS_LATENT: encoding[Output.ANCHORS_LATENT],
             Output.INV_LATENTS: x[AttentionOutput.SIMILARITIES],
-            Output.INT_PREDICTIONS: torch.argmax(out, dim=-1),
-        }
-
-
-class AbsoluteTextClassifier(nn.Module):
-    def collate_fn(self, batch: Sequence[Any]):
-        return functools.partial(self.text_encoder.text_encoder.collate_fn, batch=batch)
-
-    def __init__(
-        self,
-        metadata: MetaData,
-        text_encoder: TextEncoder,
-        batch_pre_reduce: Collection[EncodingLevel] = None,
-        anchors_reduce: Collection[EncodingLevel] = None,
-        batch_post_reduce: Collection[EncodingLevel] = None,
-        **kwargs,
-    ) -> None:
-        """Simple model that uses convolutions.
-
-        Args:
-            metadata: the metadata object
-        """
-        super().__init__()
-        pylogger.info(f"Instantiating <{self.__class__.__qualname__}>")
-
-        self.metadata = metadata
-        self.text_encoder: TextEncoder = instantiate(text_encoder)
-
-        self.text_encoder.add_stopwords(stopwords=metadata.stopwords)
-
-        n_classes: int = len(self.metadata.class_to_idx)
-
-        self.pre_reduce: Set[EncodingLevel] = (
-            set(batch_pre_reduce) if batch_pre_reduce is not None and len(batch_pre_reduce) > 0 else {}
-        )
-        self.anchors_reduce: Set[EncodingLevel] = (
-            set(anchors_reduce) if anchors_reduce is not None and len(anchors_reduce) > 0 else {}
-        )
-        self.post_reduce: Set[EncodingLevel] = (
-            set(batch_post_reduce) if batch_post_reduce is not None and len(batch_post_reduce) > 0 else {}
-        )
-
-        anchors = self.text_encoder.collate_fn(batch=metadata.anchor_samples)
-
-        if not self.text_encoder.trainable:
-            self.register_buffer("anchor_encodings", anchors["encodings"])
-            self.register_buffer("anchor_words_per_text", anchors["sections"]["words_per_text"])
-            self.register_buffer("anchor_words_per_sentence", anchors["sections"]["words_per_sentence"])
-            self.register_buffer("anchor_sentences_per_text", anchors["sections"]["sentences_per_text"])
-
-        # assert len(set.intersection(self.pre_reduce, self.post_reduce)) == 0
-        assert all(x < y for x, y in itertools.product(self.pre_reduce, self.post_reduce))
-
-        self.sequential = nn.Sequential(
-            nn.Linear(in_features=300, out_features=300),
-            DeepProjection(
-                in_features=300,
-                out_features=n_classes,
-                dropout=0.1,
-                num_layers=1,
-                activation=nn.Tanh(),
-            ),
-            nn.ReLU(),
-        )
-
-    def set_finetune_mode(self):
-        pass
-
-    def encode(self, batch, device: Device):
-        if "encodings" not in batch:
-            batch = to_device(self.text_encoder.collate_fn(batch=batch), device=device)
-        x = batch["encodings"]
-
-        x, reduced_to_sentence = EncodingLevel.reduce(
-            encodings=x, **batch["sections"], reduced_to_sentence=False, reduce_transformations=self.pre_reduce
-        )
-
-        return {"x": x, "reduced_to_sentence": reduced_to_sentence, "sections": batch["sections"]}
-
-    def decode(self, **encoding):
-        out = self.sequential(encoding["x"])
-
-        out, _ = EncodingLevel.reduce(
-            encodings=out,
-            **encoding["sections"],
-            reduced_to_sentence=encoding["reduced_to_sentence"],
-            reduce_transformations=self.post_reduce,
-        )
-
-        return {
-            Output.LOGITS: out,
-            Output.DEFAULT_LATENT: EncodingLevel.reduce(
-                encodings=encoding["x"],
-                **encoding["sections"],
-                reduced_to_sentence=encoding["reduced_to_sentence"],
-                reduce_transformations=self.post_reduce,
-            )[0],
-            Output.BATCH_LATENT: encoding["x"],
             Output.INT_PREDICTIONS: torch.argmax(out, dim=-1),
         }
 
