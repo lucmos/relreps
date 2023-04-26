627ed13c0ee3c93c6e82336478d5f5676a20bfa9
Luca Moschella <luca.moschella94@gmail.com>
2023-04-26 14:22:20 +0200

    Remove dev scripts

--- exec.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-./sync.sh "$1"
-echo ssh -tt $1 'cd rae; conda activate rae; python src/rae/run.py'
-ssh -tt $1 'cd rae; conda activate rae; python src/rae/run.py'
--- sync.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rsync -vhra /home/luca/Repos/rae/ "$1":~/rae --exclude-from='/home/luca/Repos/rae/.rsyncignore'
